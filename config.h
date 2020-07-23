b436a9e7c082f52e3270c029b7d9357d52e3d664
John McAvoy <j-mcavoy@protonmail.com>
2020-07-23 19:11:19 -0400

    Move newsboat and podboat to tag 4

--- config.h
+++ config.h
@@ -45,16 +45,18 @@ static const Rule rules[] = {
      *  WM_CLASS(STRING) = instance, class
      *  WM_NAME(STRING) = title
      */
-    /* class    instance      title          tags mask    isfloating   isterminal  noswallow  monitor */
-    { "Gimp",        NULL,       NULL,      1 << 8,       0,           0,         0,        -1 },
-    { "Thunderbird", NULL,       NULL,       1 << 3,       0,           0,         0,        -1 },
-    { "gPodder",     NULL,       NULL,       1 << 3,       0,           0,         0,        -1 },
-    { "cura",        NULL,       NULL,       1 << 5,       0,           0,         0,        -1 },
-    { "Discord",     NULL,       NULL,       1 << 8,       0,           0,         0,        -1 },
-    { "St",          NULL,       NULL,       0,            0,           1,         0,        -1 },
-    { NULL,          NULL,       "Event Tester",0,            0,           0,         1,        -1 },
-    { NULL,         "spterm",    NULL,          SPTAG(0),     1,           1,         0,        -1 },
-    { NULL,         "spcalc",    NULL,          SPTAG(1),     1,           1,         0,        -1 },
+    /* class         instance    title           tags mask     isfloating   isterminal  noswallow  monitor */
+    { "Gimp",        NULL,       NULL,           1 << 8,       0,           0,          0,        -1 },
+    { "Thunderbird", NULL,       NULL,           1 << 3,       0,           0,          0,        -1 },
+    { "gPodder",     NULL,       NULL,           1 << 3,       0,           0,          0,        -1 },
+    { "cura",        NULL,       NULL,           1 << 5,       0,           0,          0,        -1 },
+    { "Discord",     NULL,       NULL,           1 << 8,       0,           0,          0,        -1 },
+    { "St",          NULL,       NULL,           0,            0,           1,          0,        -1 },
+    { "newsboat",    NULL,       NULL,           1 << 3,       0,           1,          0,        -1 },
+    { "podboat",     NULL,       NULL,           1 << 3,       0,           1,          0,        -1 },
+    { NULL,          NULL,       "Event Tester", 0,            0,           0,          1,        -1 },
+    { NULL,         "spterm",    NULL,           SPTAG(0),     1,           1,          0,        -1 },
+    { NULL,         "spcalc",    NULL,           SPTAG(1),     1,           1,          0,        -1 },
 };
 
 /* layout(s) */
