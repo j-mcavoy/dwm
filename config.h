1fa58dd8cd0bc3f8dcad8a19ef67fc2d1308caca
John McAvoy <j-mcavoy@protonmail.com>
2020-07-25 14:43:20 -0400

    Add podboat shortcut

--- config.h
+++ config.h
@@ -189,6 +189,7 @@ static Key keys[] = {
     /* { MODKEY|ShiftMask,    XK_b,    spawn,    SHCMD("") }, */
 { MODKEY,      XK_n,    spawn,    SHCMD("st -e nvim -c VimwikiIndex") },
 { MODKEY|ShiftMask,    XK_n,    spawn,    SHCMD("st -e newsboat; pkill -RTMIN+6 dwmblocks") },
+{ MODKEY|ShiftMask,    XK_p,    spawn,    SHCMD("st -e podboat") },
 { MODKEY,      XK_m,    spawn,    SHCMD("st -e ncmpcpp") },
 { MODKEY|ShiftMask,    XK_m,    spawn,    SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
 
