cadda2a1d34c39d92c44d2819b53ee1d6c295143
John McAvoy <j-mcavoy@protonmail.com>
2020-09-21 00:32:42 -0400

    Add -a to podboat command

--- config.h
+++ config.h
@@ -53,7 +53,7 @@ static const Rule rules[] = {
     { "Discord",     NULL,       NULL,           1 << 8,       0,           0,          0,        -1 },
     { "St",          NULL,       NULL,           0,            0,           1,          0,        -1 },
     { "newsboat",    NULL,       NULL,           1 << 3,       0,           1,          0,        -1 },
-    { "podboat",     NULL,       NULL,           1 << 3,       0,           1,          0,        -1 },
+    { "podboat -a",  NULL,       NULL,           1 << 3,       0,           1,          0,        -1 },
     { NULL,          NULL,       "Event Tester", 0,            0,           0,          1,        -1 },
     { NULL,         "spterm",    NULL,           SPTAG(0),     1,           1,          0,        -1 },
     { NULL,         "spcalc",    NULL,           SPTAG(1),     1,           1,          0,        -1 },
